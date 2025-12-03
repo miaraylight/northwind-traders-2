000bcfdd7bc6f725930d7b8bafb73ba6f58dcc1e
miaraylight <miaraylight@gmail.com>
2025-12-03 10:48:46 -0600

    add connection to northwind db using dataSource

---
+++ src/main/java/com/northwind/App.java
@@ -0,0 +1,40 @@
+package com.northwind;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hi " + args[0] + args[1]);
+
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
+        dataSource.setUsername(args[0]);
+        dataSource.setPassword(args[1]);
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(
+                             "SELECT CompanyName, ContactName FROM customers " +
+                                     "WHERE CompanyName LIKE ?");
+        ) {
+            preparedStatement.setString(1, "%ba%");
+            try (ResultSet resultSet = preparedStatement.executeQuery()
+            ) {
+                while (resultSet.next()) {
+                    System.out.printf("name = %s, name2 = %s;\n",
+                            resultSet.getString(1), resultSet.getString(2));
+                }
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
